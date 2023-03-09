8c69e9d9917b3cedb97ace6cd1d578164da98247
rock_laptop <rock.in.the.shell@gmail.com>
2023-03-10 01:10:09 +0900

    AlarmScreenActivity was fixed what when expression

--- app/src/main/java/com/example/alarm/view/AlarmScreenActivity.kt
+++ app/src/main/java/com/example/alarm/view/AlarmScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.example.alarm.view
 
+import android.app.Activity
 import android.content.Context
 import android.icu.util.Calendar
 import android.media.Ringtone
@@ -10,7 +11,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
@@ -18,20 +18,17 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import com.example.alarm.R
-import com.example.alarm.home.Routes
 
 class AlarmScreenActivity: ComponentActivity() {
     // TODO: code
@@ -41,8 +38,9 @@ class AlarmScreenActivity: ComponentActivity() {
             Log.d("AlarmScreenActivity","onCreate")
             val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
             val ringTone: Ringtone = RingtoneManager.getRingtone(applicationContext,uri)
+            val activity = LocalContext.current as Activity
             ringTone.play()
-            AlarmScreen(ringTone,applicationContext)
+            AlarmScreen(ringTone,applicationContext,activity)
         }
     }
 }
@@ -51,7 +49,8 @@ class AlarmScreenActivity: ComponentActivity() {
 @Composable
 fun AlarmScreen(
     ringtone: Ringtone,
-    applicationContext: Context
+    applicationContext: Context,
+    activity: Activity
 ){
     val keyboardController = LocalSoftwareKeyboardController.current
     var inputDayOfWeek by remember { mutableStateOf("") }
@@ -86,9 +85,9 @@ fun AlarmScreen(
             Button(
                 modifier = Modifier.fillMaxWidth(),
                 onClick = {
-                    val calendar: Calendar = Calendar.getInstance()
+                    val calendar = Calendar.getInstance()
                     var todayOfWeek: String? = null
-                    when(Calendar.DAY_OF_WEEK) {
+                    when(calendar.get(Calendar.DAY_OF_WEEK)) {
                         1 -> todayOfWeek = "日曜日"
                         2 -> todayOfWeek = "月曜日"
                         3 -> todayOfWeek = "火曜日"
@@ -97,9 +96,10 @@ fun AlarmScreen(
                         6 -> todayOfWeek = "金曜日"
                         7 -> todayOfWeek = "土曜日"
                     }
-                    if (inputDayOfWeek != null){
+                    if (inputDayOfWeek != ""){
                         if (todayOfWeek + R.string.week == inputDayOfWeek){
                             ringtone.stop()
+                            activity.finish()
                         }
                         else{
                             val toast = Toast.makeText(applicationContext, R.string.youbi_is_different,Toast.LENGTH_SHORT)
